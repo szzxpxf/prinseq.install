2c39e5b479e882b2154aabf743b19e009169f15e
Eduardo Pareja-Tobes <eduardo@pareja-tobes.name>
2012-09-13 17:36:22 +0200

    fix file mods

--- install.sh
+++ install.sh
@@ -18,11 +18,11 @@ yum install -y cairo cairo-devel perl-Pod-*
 
 # install specific math matrix-real version
 /usr/local/bin/cpanm http://search.cpan.org/CPAN/authors/id/L/LE/LETO/Math-MatrixReal-2.05.tar.gz
-/usr/local/bin/cpanm Module::Build Data::Dumper::Names Time::HiRes cpanm CPAN::Meta ExtUtils::ParseXS Getopt::Long File::Temp Digest::MD5 Version::Requirements Fcntl Cwd JSON Cairo Statistics::PCA MIME::Base64
+/usr/local/bin/cpanm Module::Build Data::Dumper::Names Time::HiRes CPAN::Meta ExtUtils::ParseXS Getopt::Long File::Temp Digest::MD5 Version::Requirements Fcntl Cwd JSON Cairo Statistics::PCA MIME::Base64
 # download prinseq, install
 wget http://resources.ohnosequences.com/prinseq/prinseq-lite-0.19.4.tar.gz
 tar -xzf prinseq-lite-0.19.4.tar.gz
 rm prinseq-lite-0.19.4.tar.gz
-chmod u+x prinseq-lite-0.19.4/*.pl
+chmod +x prinseq-lite-0.19.4/*.pl
 # links
 ln -sf $BASE_FOLDER/prinseq-lite-0.19.4/*.pl /usr/bin/
